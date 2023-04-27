7f4a85e9991c43ae7fa69aad861b8b8cc3c69f07
sifat18 <ibnsifat@gmail.com>
2023-04-28 04:27:26 +0600

    user route issue fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,5 @@ const router = express.Router();
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
 // update
-router.patch("/:id", verifyToken, getUser);
-router.get("/:id/:friendId", verifyToken, addRemoveFriend);
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 export default router;
